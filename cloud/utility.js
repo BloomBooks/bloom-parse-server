77dc46cbe456b45aa9a1ae808daef900c30f9059
Andrew Polk <andrew_polk@sil.org>
2023-07-26 15:25:59 -0700

    Better handling of special characters (BL-12326)

--- cloud/utility.js
+++ cloud/utility.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-undef */
-
 // This is just a little utility function used to set the langTag on the show field for epub and pdfs.
 // It is intended as a one-off run to fix up existing books. Starting July 2023, the harvester sets this
 // for every book it processes, using the actual contentLanguage1 in the dom.
+// eslint-disable-next-line no-undef
 Parse.Cloud.define("setArtifactLangTags", async (request) => {
     request.log.info("setArtifactLangTags - Starting.");
 
@@ -12,38 +11,58 @@ Parse.Cloud.define("setArtifactLangTags", async (request) => {
     }
 
     // Query for all books
+    // eslint-disable-next-line no-undef
     var query = new Parse.Query("books");
     query.select("title", "allTitles", "show");
     await query.each((book) => {
         book.set("updateSource", "setArtifactLangTags"); // very important that we don't leave updateSource unset so we don't add system:incoming tag
 
-        const allTitlesJson = book.get("allTitles"); // allTitles is a string field
-        const title = book.get("title");
+        let allTitlesJson = book.get("allTitles"); // allTitles is a string field
+        let title = book.get("title");
 
-        if (!allTitlesJson || !title) {
+        if (!allTitlesJson || !title || allTitlesJson === "{}") {
             request.log.info(
                 `setArtifactLangTags found missing allTitles or title for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`.`
             );
             return; // continue
         }
 
+        // Trust me; it's worth the slight loss of integrity to not have to deal with these special characters
+        // which are not handled consistently in our data (actual new line, escaped new line, quotes).
+        allTitlesJson = allTitlesJson
+            .replace(/\r/g, "")
+            .replace(/\n/g, "")
+            .replace(/\\n/g, "")
+            // quote will be escaped in json
+            .replace(/\\"/g, "")
+            // sigh; even one of these...
+            .replace(/<br \/>/, "");
+        title = title
+            .replace(/\r/g, "")
+            .replace(/\n/g, "")
+            .replace(/\\n/g, "")
+            // if quote is escaped in title, it looks like \\"
+            .replace(/\\\\"/g, "")
+            // but most are not escaped
+            .replace(/"/g, "")
+            .trim();
+
         // Parse the JSON string to a JavaScript object
         let allTitles;
-        let allTitlesJsonEscaped;
         try {
-            // This is confusing, but if we find \" or a control character, we need to escape so that parsing will give us back what we started with.
-            // Adding to the confusion, each \ in the replacement string has to, itself, be escaped.
-            allTitlesJsonEscaped = allTitlesJson
-                .replace(/\\"/g, '\\\\\\"') // \" -> \\\"
-                .replace(/\n/g, "\\n")
-                .replace(/\r/g, "\\r")
-                .replace(/\t/g, "\\t");
-            allTitles = JSON.parse(allTitlesJsonEscaped);
+            allTitles = JSON.parse(allTitlesJson);
         } catch (e) {
             request.log.error(
-                `setArtifactLangTags failed to parse allTitlesJson for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`, allTitlesJsonEscaped \`${allTitlesJsonEscaped}\`.`
+                `setArtifactLangTags failed to parse allTitlesJson for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`.`
             );
             return; // continue
+        }
+
+        const allTitlesKeys = Object.keys(allTitles);
+        if (allTitlesKeys.length === 1) {
+            // Only one entry in allTitles; take the easy, quick out and use it.
+            setShowLangTag(book, allTitlesKeys[0], dryRun, request);
+            return;
         }
 
         const languageTags = getKeysByValue(allTitles, title);
@@ -54,28 +73,11 @@ Parse.Cloud.define("setArtifactLangTags", async (request) => {
             return; // continue
         } else if (languageTags.length === 0) {
             request.log.info(
-                `setArtifactLangTags failed to find a languageTag for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`, allTitlesJsonEscaped \`${allTitlesJsonEscaped}\`.`
+                `setArtifactLangTags failed to find a languageTag for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`.`
             );
             return; // continue
         } else {
-            if (dryRun) {
-                request.log.info(
-                    `setArtifactLangTags setting show langTag to \`${languageTags[0]}\` for book \`${book.id}\` with title \`${title}\` and allTitles \`${allTitlesJson}\`.`
-                );
-            }
-
-            let show = book.get("show");
-            //console.log("show before: " + JSON.stringify(show));
-
-            ["epub", "pdf"].forEach((artifactType) => {
-                if (!show) show = {};
-                if (!show[artifactType]) show[artifactType] = {};
-
-                show[artifactType].langTag = languageTags[0];
-            });
-            //console.log("show after: " + JSON.stringify(show));
-
-            book.set("show", show);
+            setShowLangTag(book, languageTags[0], dryRun, request);
         }
 
         try {
@@ -91,7 +93,34 @@ Parse.Cloud.define("setArtifactLangTags", async (request) => {
     request.log.info("setArtifactLangTags - Completed successfully.");
 });
 
+function setShowLangTag(book, langTag, dryRun, request) {
+    if (dryRun) {
+        request.log.info(
+            `setArtifactLangTags setting show langTag to \`${langTag}\` for book ` +
+                `\`${book.id}\` with title \`${book.get("title")}\` ` +
+                `and allTitles \`${book.get("allTitles")}\`.`
+        );
+    }
+
+    let show = book.get("show");
+    //request.log.info("show before: " + JSON.stringify(show));
+
+    ["epub", "pdf"].forEach((artifactType) => {
+        if (!show) show = {};
+        if (!show[artifactType]) show[artifactType] = {};
+
+        show[artifactType].langTag = langTag;
+    });
+    //request.log.info("show after: " + JSON.stringify(show));
+
+    // In theory, we shouldn't have to switch for dryRun here, but something was causing strange behavior on the real server.
+    // And my only theory is that it is trying to keep all these changes in memory.
+    if (!dryRun) {
+        book.set("show", show);
+    }
+}
+
 // Define a function that returns array of keys by value
 function getKeysByValue(object, value) {
-    return Object.keys(object).filter((key) => object[key] === value);
+    return Object.keys(object).filter((key) => object[key].trim() === value);
 }
